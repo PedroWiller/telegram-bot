4fcd20cc81e6735dfedb5a31692079f9e47fd996
pedrowiller <pedrowiller25@gmail.com>
2025-03-04 18:47:37 -0300

    chore: set new values in yaml file

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Sprintln("Hello, World!")
+
+	app := fiber.New()
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	app.Listen(":8080")
 }
